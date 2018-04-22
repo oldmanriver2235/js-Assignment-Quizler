dac27a6cbc513bd0dde525f6ed9c2c4e92a6334b
wmarttala <wmarttala123@gmail.com>
2018-04-22 01:11:31 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import vorpal from 'vorpal'
-import inquirer from 'inquirer'
+import { prompt } from 'inquirer'
 
-import { readFile, writeFile, chooseRandom, createPrompt, createQuestions } from './lib'
+import {
+  readFile,
+  writeFile,
+  chooseRandom,
+  createPrompt,
+  createQuestions
+} from './lib'
 
 const cli = vorpal()
 
@@ -10,7 +16,7 @@ const askForQuestions = [
     type: 'input',
     name: 'numQuestions',
     message: 'How many questions do you want in your quiz?',
-    validate: (input) => {
+    validate: input => {
       const pass = input.match(/^[1-9]{1}$|^[1-9]{1}[0-9]{1}$|^100$/)
       return pass ? true : 'Please enter a valid number!'
     }
@@ -19,42 +25,55 @@ const askForQuestions = [
     type: 'input',
     name: 'numChoices',
     message: 'How many choices should each question have?',
-    validate: (input) => {
+    validate: input => {
       const pass = input.match(/^(?:[2-4]|0[2-4]|4)$/)
       return pass ? true : 'Please enter a valid number!'
     }
   }
 ]
 
-const createQuiz = (title) =>
-  // TODO implement createQuiz
+const createQuiz = title =>
+  prompt(askForQuestions)
+    .then(answer =>
+      // TODO finish createQuiz logic
+      console.log(answer)
+    )
+    .catch(err => console.log('Error creating the quiz.', err))
 
-const takeQuiz = (title, output) =>
-  // TODO implement takeQuiz
+// const takeQuiz = (title, output) =>
+// TODO implement takeQuiz
 
-const takeRandomQuiz = (quizes, output, n) =>
-  // TODO implement takeRandomQuiz
+// const takeRandomQuiz = (quizes, output, n) =>
+// TODO implement takeRandomQuiz
 
 cli
-  .command('create <fileName>', 'Creates a new quiz and saves it to the given fileName')
+  .command(
+    'create <fileName>',
+    'Creates a new quiz and saves it to the given fileName'
+  )
   .action(function (input, callback) {
-    // TODO implement functionality for create command
+    // TODO update create command for correct functionality
+    return createQuiz(input.fileName)
   })
 
 cli
-  .command('take <fileName> <outputFile>', 'Loads a quiz and saves the users answers to the given outputFile')
+  .command(
+    'take <fileName> <outputFile>',
+    'Loads a quiz and saves the users answers to the given outputFile'
+  )
   .action(function (input, callback) {
     // TODO implement functionality for taking a quiz
   })
 
 cli
-  .command('random <outputFile> <fileNames...>', 'Loads a quiz or' +
-    ' multiple quizes and selects a random number of questions from each quiz.' +
-    ' Then, saves the users answers to the given outputFile')
+  .command(
+    'random <outputFile> <fileNames...>',
+    'Loads a quiz or' +
+      ' multiple quizes and selects a random number of questions from each quiz.' +
+      ' Then, saves the users answers to the given outputFile'
+  )
   .action(function (input, callback) {
     // TODO implement the functionality for taking a random quiz
   })
 
-cli
-  .delimiter(cli.chalk['yellow']('quizler>'))
-  .show()
+cli.delimiter(cli.chalk['yellow']('quizler>')).show()
